4f17f01918b3336483411ae2531d294103396934
GT <55749965+gtiralongo@users.noreply.github.com>
2024-06-07 21:44:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def edit_player_page():
         st.warning("No hay jugadores disponibles para editar.")
         return
 
-    player_names = [player["name"] for player in data]
+    player_names = [data[player]["name"] for player in data]
     selected_player = st.selectbox("Selecciona un jugador para editar", player_names)
     player_data = next(player for player in data if player["name"] == selected_player)
 
