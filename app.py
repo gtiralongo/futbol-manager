2c31b496216d8c2e59435c897e741dfb31c9b055
GT <55749965+gtiralongo@users.noreply.github.com>
2024-06-10 10:06:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def show_players_page():
         return
 
     for key, player in data.items():
-        df = pd.DataFrame(data)
+        df = pd.DataFrame(data.keys())
         st.table(df)
         # st.write(f"Nombre: {player['name']}, Velocidad: {player['velocidad']}, Defensa: {player['defensa']}, Ataque: {player['ataque']}, Posición: {player['posición']}")
 
