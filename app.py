16663966b1afe12825e56d7b4eaf615ef1889043
GT <55749965+gtiralongo@users.noreply.github.com>
2024-06-10 16:08:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def edit_player_page():
     # posición = st.selectbox("Posición", ["Delantero", "Defensa", "Mediocampista", "Arquero"], index=["Delantero", "Defensa", "Mediocampista", "Arquero"].index(player_data["posición"]))
     col1, col2 = st.columns(3)
     with col1:
-        selected_player = st.selectbox("Selecciona un jugador para editar", player_names
+        selected_player = st.selectbox("Selecciona un jugador para editar", player_names)
         player_data = next(data[player] for player in data if data[player]["name"] == selected_player)
     with col2:
         posición = st.selectbox("Posición", ["Delantero", "Defensa", "Mediocampista", "Arquero"], index=["Delantero", "Defensa", "Mediocampista", "Arquero"].index(player_data["posición"]))
