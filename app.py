df399d8664e6be6ce1c50daf698553aa2ae62b8a
GT <55749965+gtiralongo@users.noreply.github.com>
2024-06-10 14:24:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -221,7 +221,7 @@ def add_and_select_teams_page():
 
 # Página de bienvenida
 def welcome_page():
-    st.write("keys:", st.secrets["key"]["secrets"])
+    st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
     st.title("Bienvenido a la Selección de Jugadores")
     st.write("Esta aplicación te ayudará a seleccionar jugadores para dos equipos basados en sus características.")
     show_players_page()
